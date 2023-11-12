ce243712f295c041c9e8c340b35079fd85ea68cc
unknown <kauegallegosouza@gmail.com>
2023-11-12 01:33:51 -0300

    First commit Branch

---
+++ 02-EstruturasDecisao/src/br/com/treinamento/For.java
@@ -0,0 +1,14 @@
+package br.com.treinamento;
+
+public class For {
+	
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i = 0; i < 5; i++) {
+		    System.out.println(i+1);
+		}
+	}
+
+}
