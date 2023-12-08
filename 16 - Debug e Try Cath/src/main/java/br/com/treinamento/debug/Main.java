f41a327d0ea18be3dcb5b9191a25a59dbb694b62
GallegoDev <kauegallegosouza@gmail.com>
2023-12-08 19:52:20 -0300

    Debug e Try Cath

---
+++ 16 - Debug e Try Cath/src/main/java/br/com/treinamento/debug/Main.java
@@ -0,0 +1,43 @@
+package br.com.treinamento.debug;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("Inicio do método main.");
+		
+		metodo1(5);
+		
+		System.out.println("Fim do método main.");
+
+	}
+
+	private static void metodo1(Integer imposto) throws Exception {
+		
+		System.out.println("Inicio do método 1.");
+		
+		if(imposto<3) {
+			Exception ex = new Exception("O valor não pode ser menor que 3.");
+			throw ex;
+		}
+		
+		metodo2();
+		
+		System.out.println("Fim do método 1.");
+
+	}
+
+	private static void metodo2() {
+
+		System.out.println("Inicio do método 2.");
+		
+		for(int i=0;i<5;i++) {
+			System.out.println("Valor de i: " +i);
+		}
+		
+		System.out.println("Fim do método 2.");
+
+		
+	}
+	
+}
